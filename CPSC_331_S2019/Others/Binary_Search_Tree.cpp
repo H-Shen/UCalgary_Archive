88a8964a9d0ebbb6d9ec316e5da8da73c67ff06f
Haohu Shen <haohu.shen@ucalgary.ca>
2020-01-26 00:33:16 -0700

    updated

--- CPSC_331_S2019/Others/Binary_Search_Tree.cpp
+++ CPSC_331_S2019/Others/Binary_Search_Tree.cpp
@@ -26,6 +26,7 @@ namespace Binary_search_tree {
     // Return true if the BST includes a node whose value is given,
     // otherwise return false
     template<typename T>
+    inline
     bool search(const std::shared_ptr<Node<T> > &root, const T &value) {
         // Case 1:
         if (!root) {
@@ -44,6 +45,7 @@ namespace Binary_search_tree {
     }
 
     template <typename T>
+    inline
     void pre_order_traverse(const std::shared_ptr<Node<T> > &root) {
         if (!root) {
             return;
@@ -54,6 +56,7 @@ namespace Binary_search_tree {
     }
 
     template <typename T>
+    inline
     void in_order_traverse(const std::shared_ptr<Node<T> > &root) {
         if (!root) {
             return;
@@ -64,6 +67,7 @@ namespace Binary_search_tree {
     }
 
     template <typename T>
+    inline
     void post_order_traverse(const std::shared_ptr<Node<T> > &root) {
         if (!root) {
             return;
@@ -74,6 +78,7 @@ namespace Binary_search_tree {
     }
 
     template <typename T>
+    inline
     void level_order_traverse(const std::shared_ptr<Node<T> > &root) {
         std::queue<std::shared_ptr<Node<T> > >q;
         q.push(root);
@@ -95,6 +100,7 @@ namespace Binary_search_tree {
 
     // Check if two trees are the same(same shape, same nodes)
     template <typename T>
+    inline
     bool is_same_tree(const std::shared_ptr<Node<T> > &lhs, const std::shared_ptr<Node<T> > &rhs) {
         if (!lhs && !rhs) {
             return true;
@@ -109,8 +115,9 @@ namespace Binary_search_tree {
         return false;
     }
 
-    // Check if two trees are the same shape
+    // Check if two trees have the same shape
     template <typename T>
+    inline
     bool is_same_shape(const std::shared_ptr<Node<T> > &lhs, const std::shared_ptr<Node<T> > &rhs) {
         if (!lhs && !rhs) {
             return true;
@@ -124,6 +131,7 @@ namespace Binary_search_tree {
 
     // Obtain the maximal depth of a BST
     template <typename T>
+    inline
     int get_height(const std::shared_ptr<Node<T> > &root) {
         if (!root) {
             return 0;
@@ -133,6 +141,7 @@ namespace Binary_search_tree {
 
     // Obtain a mirror of a BST
     template <typename T>
+    inline
     void mirror(std::shared_ptr<Node<T> > &root) {
         if (!root) {
             return;
@@ -144,6 +153,7 @@ namespace Binary_search_tree {
 
     // Insert a node to the BST
     template <typename T>
+    inline
     std::shared_ptr<Node<T> > insert(std::shared_ptr<Node<T> > &root, const T &value) {
         if (!root) {
             root = std::make_shared<Node<T> >(value);
@@ -179,14 +189,143 @@ namespace Binary_search_tree {
     //    Binary_search_tree::in_order_traverse(BSTInstance);
 
     template <typename T>
+    inline
     bool empty(const std::shared_ptr<Node<T> > &root) {
         return (root == nullptr);
     }
     //  std::shared_ptr<Binary_search_tree::Node<int> > BSTInstance = nullptr;
     //  std::cout << std::boolalpha << Binary_search_tree::empty(BSTInstance) << std::endl;
+
+    // Helper function: get the successor of root(the node with the smallest value in the right subtree)
+    // Pre-condition: root has a right subtree
+    template <typename T>
+    inline
+    std::shared_ptr<Node<T> > get_successor(const std::shared_ptr<Node<T> > &root) {
+        std::shared_ptr<Node<T> > node = root->right_child;
+        while (node->left_child != nullptr) {
+            node = node->left_child;
+        }
+        return node;
+    }
+
+    // Helper function: remove the node
+    // Pre-condition: root is not null
+    template <typename T>
+    inline
+    void delete_a_node(std::shared_ptr<Node<T> > &root) {
+        if (root->left_child == nullptr) {
+            // Case 1: left is null, right is null
+            if (root->right_child == nullptr) {
+                if (root->father == nullptr) {
+                    root = nullptr;
+                } else {
+                    std::shared_ptr<Node<T> > father_node = root->father;
+                    // When the root is the left child of its father
+                    // we break the relationship between root and its father
+                    if (father_node->value > root->value) {
+                        father_node->left_child = nullptr;
+                    }
+                    // When the root is the right child of its father
+                    // we break the relationship between root and its father
+                    else {
+                        father_node->right_child = nullptr;
+                    }
+                }
+            }
+            // Case 2: left is null, right is not null
+            else {
+                std::shared_ptr<Node<T> > right_child_node = root->right_child;
+                // When root has no parents, we make its right child as new root
+                if (root->father == nullptr) {
+                    right_child_node->father = nullptr;
+                    root = right_child_node;
+                }
+                else {
+                    std::shared_ptr<Node<T> > father_node = root->father;
+                    // When root is its father's left child
+                    if (root->value < father_node->value) {
+                        father_node->left_child = right_child_node;
+                    }
+                    // When root is its father's right child
+                    else {
+                        father_node->right_child = right_child_node;
+                    }
+                    // Update the father information of the right child
+                    right_child_node->father = father_node;
+                }
+            }
+        }
+        // Case 3: left is not null, right is null
+        else if (root->right_child == nullptr) {
+            std::shared_ptr<Node<T> > left_child_node = root->left_child;
+            // When root has no parents, we make its left child as new root
+            if (root->father == nullptr) {
+                left_child_node->father = nullptr;
+                root = left_child_node;
+            }
+            else {
+                std::shared_ptr<Node<T> > father_node = root->father;
+                // When root is its father's left child
+                if (root->value < father_node->value) {
+                    father_node->left_child = left_child_node;
+                }
+                // When root is its father's right child
+                else {
+                    father_node->right_child = left_child_node;
+                }
+                left_child_node->father = father_node;
+            }
+        }
+        // Case 4: left is not null, right is not null
+        else {
+            std::shared_ptr<Node<T> > successor = get_successor(root);
+            root->value = successor->value;
+            delete_a_node(successor);
+        }
+    }
+
+    // Delete a node whose value is key
+    template <typename T>
+    inline
+    void erase(std::shared_ptr<Node<T> > &root, T key) {
+        // Case 1:
+        if (root == nullptr) {
+            return;
+        }
+        // Case 2:
+        if (key < root->value) {
+            erase(root->left_child, key);
+        }
+        // Case 3:
+        else if (key > root->value) {
+            erase(root->right_child, key);
+        }
+        // Case 4:
+        else {
+            delete_a_node<T>(root);
+        }
+    }
+    //    std::vector<int> A = {2, 4, 1, 7, 8, 3};
+    //    std::shared_ptr<Binary_search_tree::Node<int> > Tree;
+    //    for (const auto &i : A) {
+    //        Tree = Binary_search_tree::insert(Tree, i);
+    //    }
+    //    Binary_search_tree::erase(Tree, 0);
+    //    Binary_search_tree::in_order_traverse(Tree);
+
+    // Get the number of nodes in the BST
+    // An alternative way is storing the size as a data member, thus
+    // we can obtain the number of nodes by root->size in O(1)
+    template <typename T>
+    inline
+    int get_size(std::shared_ptr<Node<T> > &root) {
+        if (!root) {
+            return 0;
+        }
+        return get_size(root->left_child) + get_size(root->right_child) + 1;
+    }
 }
 
 int main() {
-
     return 0;
 }
